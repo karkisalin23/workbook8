30a141d964ea4b752039903c596a97b8a434bea9
SALIN KARKI <karkisalin22@yahoo.com>
2023-12-15 11:30:53 -0500

    update

--- workshop/scripts/todos.js
+++ workshop/scripts/todos.js
@@ -1,26 +1,38 @@
-fetch("http://localhost:8083/api/users")
-  .then((response) => response.json())
-  .then((users) => {
-    const userDropdown = document.getElementById("userDropdown");
-    users.forEach((user) => {
-      const option = document.createElement("option");
-      option.value = user.id;
-      option.textContent = user.name;
-      userDropdown.appendChild(option);
-    });
-  });
+function loadTodos() {
+  const userDropdown = document.getElementById("userDropdown");
+  const todoList = document.getElementById("todoList");
 
-userDropdown.addEventListener("change", () => {
-  const selectedUserId = userDropdown.value;
-  fetch(`http://localhost:8083/api/todos/byuser/${selectedUserId}`)
+  // Fetch users and populate dropdown
+  fetch("http://localhost:8083/api/users")
     .then((response) => response.json())
-    .then((todos) => {
-      const todoList = document.getElementById("todoList");
-      todoList.innerHTML = "";
-      todos.forEach((todo) => {
-        const todoItem = document.createElement("div");
-        todoItem.textContent = `Description: ${todo.description}, Deadline: ${todo.deadline}`;
-        todoList.appendChild(todoItem);
+    .then((users) => {
+      users.forEach((user) => {
+        const option = document.createElement("option");
+        option.value = user.id;
+        option.textContent = user.name;
+        userDropdown.appendChild(option);
       });
     });
-});
+
+  // Event listener for user selection
+  userDropdown.addEventListener("change", () => {
+    const selectedUserId = userDropdown.value;
+
+    // Fetch todos for the selected user
+    fetch(`http://localhost:8083/api/todos/byuser/${selectedUserId}`)
+      .then((response) => response.json())
+      .then((todos) => {
+        // Clear previous todoList content
+        todoList.innerHTML = "";
+
+        // Display todos in the todoList
+        todos.forEach((todo) => {
+          const todoItem = document.createElement("div");
+          todoItem.textContent = `Description: ${todo.description}, Deadline: ${todo.deadline}, Priority: ${todo.priority}`;
+          todoList.appendChild(todoItem);
+        });
+      });
+  });
+}
+
+loadTodos();
