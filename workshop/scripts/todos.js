0ce7ddf93111df9279ab17c9bb911101d597fc11
SALIN KARKI <karkisalin22@yahoo.com>
2023-12-15 10:50:31 -0500

    commits

--- workshop/scripts/todos.js
+++ workshop/scripts/todos.js
@@ -1,1 +1,30 @@
+function fetchAllToDos() {
+  fetch("http://localhost:8083/api/todos")
+    .then((response) => response.json())
+    .then((todos) => {
+      const todoList = document.getElementById("todoList");
+      todoList.innerHTML = "";
 
+      todos.forEach((todo) => {
+        const row = document.createElement("tr");
+        const idCell = document.createElement("td");
+        const descriptionCell = document.createElement("td");
+        const deadlineCell = document.createElement("td");
+
+        idCell.textContent = todo.id;
+        descriptionCell.textContent = todo.description;
+        deadlineCell.textContent = todo.deadline;
+
+        row.appendChild(idCell);
+        row.appendChild(descriptionCell);
+        row.appendChild(deadlineCell);
+
+        todoList.appendChild(row);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching ToDos:", error);
+    });
+}
+
+fetchAllToDos();
