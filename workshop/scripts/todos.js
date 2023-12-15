d84ff0674be609612034335128e4a7019b221ba6
SALIN KARKI <karkisalin22@yahoo.com>
2023-12-15 11:13:19 -0500

    update

--- workshop/scripts/todos.js
+++ workshop/scripts/todos.js
@@ -1,4 +1,3 @@
-"use strict";
 function fetchAllToDos() {
   fetch("http://localhost:8083/api/todos")
     .then((response) => response.json())
@@ -9,16 +8,22 @@ function fetchAllToDos() {
       todos.forEach((todo) => {
         const row = document.createElement("tr");
         const idCell = document.createElement("td");
+        const categoryCell = document.createElement("td");
         const descriptionCell = document.createElement("td");
         const deadlineCell = document.createElement("td");
+        const priorityCell = document.createElement("td");
 
         idCell.textContent = todo.id;
+        categoryCell.textContent = todo.category;
         descriptionCell.textContent = todo.description;
         deadlineCell.textContent = todo.deadline;
+        priorityCell.textContent = todo.priority;
 
         row.appendChild(idCell);
+        row.appendChild(categoryCell);
         row.appendChild(descriptionCell);
         row.appendChild(deadlineCell);
+        row.appendChild(priorityCell);
 
         todoList.appendChild(row);
       });
